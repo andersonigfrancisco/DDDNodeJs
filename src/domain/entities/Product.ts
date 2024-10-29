115527e22f507a3bce46779a02c63fa6cabf0651
andersonigfrancisco <andersonfranciscoig@gmail.com>
2024-10-29 19:54:00 +0100

    feat: add new entities

---
+++ src/domain/entities/Product.ts
@@ -0,0 +1,14 @@
+class Product{
+    
+    public name: string;
+    public description: string;
+    public price: number;
+    public category: string;
+
+    constructor(name:string,description:string,price: number,category: string){
+        this.name=name;
+        this.description=description;
+        this.price=price;
+        this.category=category;
+    }
+}
---
+++ src/domain/entities/user.ts
@@ -0,0 +1,12 @@
+class User{
+    
+    public name: string;
+    public password:string;
+    public email: string;
+
+    constructor(name:string,password:string,email: string){
+        this.name=name;
+        this.password=password;
+        this.email=email;
+    }
+}
